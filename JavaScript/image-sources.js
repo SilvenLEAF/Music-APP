f6f112cc6fb0fbf10d74be2661800d19afbc5fbe
SilvenLEAF <silvenleaf@gmail.com>
2020-05-07 14:56:01 +0530

    created the draft of the container with carousel

---
+++ JavaScript/image-sources.js
@@ -0,0 +1,15 @@
+// document.querySelector('.one').src = 'images/Glory-LOGO.jpg';
+// document.querySelector('.two').src = 'images/Glory-Ye-Xiu.jpg';
+// document.querySelector('.three').src = 'images/Glory-Heroine.jpg';
+// document.querySelector('.four').src = 'images/Glory-Heroine-Friend.jpg';
+// document.querySelector('.five').src = 'images/Glory-Friend.jpg';
+// document.querySelector('.six').src = 'images/Glory-Math-Genius.jpg';
+// document.querySelector('.seven').src = 'images/Glory-Muscular.jpg';
+// document.querySelector('.eight').src = 'images/Glory-Villain.jpg';
+// document.querySelector('.nine').src = 'images/Glory-Mage-Apprentice.jpg';
+
+let arr= document.querySelectorAll('img');
+
+for (let i=0; i<arr.length; i++){
+     arr[i].src = 'images/Glory-Heroine-Friend.jpg';
+}
