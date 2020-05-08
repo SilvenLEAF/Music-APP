10419855691475b6b342a54d68be1d6032f88a8d
SilvenLEAF <silvenleaf@gmail.com>
2020-05-08 11:27:11 +0530

    v1 of Music APP. Without SETTINGS

--- JavaScript/buttons-and-songs.js
+++ JavaScript/buttons-and-songs.js
@@ -1,22 +1,65 @@
 let songList = [
      {
+          name: 'クロスウォーズ',
+          image: 'anime/xwars.jpg',
+          time: '4:18',
+          song: 'songs/xwars.mp3'
+     },
+     {
+          name: '未来の夢',
+          image: 'anime/dm.jpg',
+          time: '4:04',
+          song: 'songs/dm.mp3'
+     },
+     {
           name: 'ピースサイン',
           image: 'anime/bha.jpg',
           time: '4:02',
           song: 'songs/peace-sign.mp3'
      },
      {
-          name: 'Fly HIGH',
-          image: 'anime/hkyu.jpg',
-          time: '5:07',
-          song: 'songs/fly-high.mp3'
+          name: '東京喰種',
+          image: 'anime/tg.jpg',
+          time: '3:55',
+          song: 'songs/tg.mp3'
      },
      {
-          name: '世界最強',
+          name: 'ドクターストーン',
           image: 'anime/ds.jpg',
           time: '4:08',
           song: 'songs/s3.mp3'
+     },
+     {
+          name: '若鶏烏',
+          image: 'anime/hkyu.jpg',
+          time: '4:02',
+          song: 'songs/fly-high.mp3'
+     },
+     {
+          name: 'ガチンコ',
+          image: 'anime/bbg.jpg',
+          time: '0:45',
+          song: 'songs/bbg.mp3'
+     },
+     {
+          name: '世界最強',
+          image: 'anime/opm.jpg',
+          time: '3:47',
+          song: 'songs/hero.mp3'
+     },
+     {
+          name: 'ポケットモンスター',
+          image: 'anime/pk.jpg',
+          time: '4:17',
+          song: 'songs/pk.mp3'
+     },
+     {
+          name: '僕等二人',
+          image: 'anime/gto.png',
+          time: '3:47',
+          song: 'songs/gto.mp3'
      }
+
 ]
 /* *******************************************************
 ******************************************************** */
@@ -29,22 +72,12 @@ let songNameArr= document.querySelectorAll('.song-name');
 /* *******************************************************
 .                                     SONG LIST NAME+TIME
 ******************************************************** */
-     //------------default iconArr
-     for (let i=0; i<iconArr.length; i++){
-          iconArr[i].src = 'images/Glory-LOGO.jpg';
-     }
-     //-----------------------------------------
 
-
-songNameArr[0].textContent = songList[0].name;
-songTimeArr[0].textContent = songList[0].time;
-
-songNameArr[1].textContent = songList[1].name;
-songTimeArr[1].textContent = songList[1].time;
-
-songNameArr[2].textContent = songList[2].name;
-songTimeArr[2].textContent = songList[2].time;
-
+for(let i=0; i<songList.length; i++){
+     songNameArr[i].textContent = songList[i].name;
+     songTimeArr[i].textContent = songList[i].time;
+     iconArr[i].style.backgroundImage = `url(${songList[i].image})`;
+}
 
 /* *******************************************************
 ******************************************************** */
